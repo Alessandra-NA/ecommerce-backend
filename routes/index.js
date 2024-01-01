4650e8429079101ffdf193bfc5801f7dd0375ccc
Alessandra Nuñez <alessandra.na001@gmail.com>
2024-01-01 16:40:24 -0500

    wip: routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
-const { createUser } = require('../controllers/user.controller');
-
+const { createUser, updateUser } = require('../controllers/user.controller');
+const { addProductCart, deleteProductCart, getCartInfo } = require('../controllers/shoppingcart.controller');
+const { createProduct, getProductInfo, updateProductInfo, deleteProduct, getSaleProducts  } = require('../controllers/product.controller');
 
 
 const router = Router();  
@@ -10,5 +11,16 @@ router.get('/', (req, res) => {
 })
 
 router.get('/user/create', createUser)
+router.get('/user/update', updateUser)
+
+router.get('/cart/addProduct', addProductCart)
+router.get('/cart/deleteProduct', deleteProductCart)
+router.get('/cart/get', getCartInfo)
+
+router.get('/product/create', createProduct)
+router.get('/product/get', getProductInfo)
+router.get('/product/update', updateProductInfo)
+router.get('/product/delete', deleteProduct)
+router.get('/product/getSale', getSaleProducts)
 
 module.exports = router;
